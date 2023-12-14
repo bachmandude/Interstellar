f76ed1af1d773696ebffa4a09ef441370f7d8370
bachmandude <117601487+bachmandude@users.noreply.github.com>
2023-12-13 21:36:44 -0600

    Update home.js

--- static/assets/scripts/home.js
+++ static/assets/scripts/home.js
@@ -6,6 +6,33 @@ try {
     inFrame = true;
 };
 
+if (!inFrame && !navigator.userAgent.includes("Firefox")) {
+    const popup = open("about:blank", "_blank")
+    if (!popup || popup.closed) alert("Please allow popups and redirects.")
+    else {
+        const doc = popup.document
+        const iframe = doc.createElement("iframe")
+        const style = iframe.style
+        const link = doc.createElement("link")
+
+        const name = localStorage.getItem("name") || "IXL | Math, Language Arts, Science";
+        const icon = localStorage.getItem("icon") || "https://www.ixl.com/ixl-favicon.png";
+
+        doc.title = name;
+        link.rel = "icon";
+        link.href = icon;
+
+        iframe.src = location.href 
+        style.position = "fixed"
+        style.top = style.bottom = style.left = style.right = 0
+        style.border = style.outline = "none"
+        style.width = style.height = "100%"
+
+        doc.head.appendChild(link);
+        doc.body.appendChild(iframe);
+        location.replace(localStorage.getItem('panicLink') || 'https://www.ixl.com/');
+    };
+};
 
 document.addEventListener("DOMContentLoaded", function(event) { 
     if (window.localStorage.getItem("v4Particles") == "true") {
